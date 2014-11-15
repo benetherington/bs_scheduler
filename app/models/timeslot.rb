8824e52798f0c358aa808792b81820c5ba388cb4
benetherington <ben.j.etherington@gmail.com>
2014-11-14 20:59:56 -0600

    restarting sessions implementation, correct new user page, permit new user parameters

--- app/models/timeslot.rb
+++ app/models/timeslot.rb
@@ -1,4 +1,5 @@
 class Timeslot < ActiveRecord::Base
+
 	belongs_to :window
 	belongs_to :user
 
--- app/models/window.rb
+++ app/models/window.rb
@@ -1,7 +1,9 @@
 class Window < ActiveRecord::Base
+
 	belongs_to :event
 	has_many :timeslots
 	has_many :users, through: :timeslots
+
 	# suggested validations:
 	# validates_associated :event
 	# validates :tasters, numerically: { less_than: :capacity }
